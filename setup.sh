dfbbe24e2179bf06552efed116677982dcc86c78
thenimas <thenimas@hotmail.com>
2025-11-18 13:45:30 -0500

    updated packages

--- setup.sh
+++ setup.sh
@@ -331,7 +331,7 @@ apt upgrade -yy
 
 # apt --fix-broken install -yy
 
-apt install locales util-linux-extra -yy
+apt install locales locales-all util-linux-extra linux-image-amd64 firmware-linux grub2 dbus ca-certificates locales man-db sudo nano efibootmgr -yy
 
 apt autoremove -yy
 
@@ -351,7 +351,7 @@ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
 # installing packages
-apt install ark bluez btrfs-progs gh git fonts-recommended fonts-ubuntu flatpak gamemode gnome-software ufw i3 kate kcalc fastfetch nitrogen nano sudo cryptsetup pavucontrol pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse plymouth plymouth-themes qdirstat virt-manager redshift-gtk rxvt-unicode timeshift thunar thunar-archive-plugin gvfs-backends ttf-mscorefonts-installer vlc x11-xserver-utils xdg-desktop-portal xserver-xorg-core nitrogen xclip playerctl xdotool pulseaudio-utils network-manager-gnome ibus lightdm tasksel curl firmware-misc-nonfree wget systemsettings systemd-zram-generator lxappearance initramfs-tools sox libsox-fmt-all lshw lxinput maim grub-efi-amd64 linux-image-amd64 nodejs default-jdk python3 gdb bc fail2ban -yy
+apt install ark bluez btrfs-progs gh git fonts-recommended fonts-ubuntu flatpak gamemode gnome-software ufw i3 kate kcalc fastfetch nitrogen cryptsetup pavucontrol pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse plymouth plymouth-themes qdirstat virt-manager redshift-gtk rxvt-unicode timeshift thunar thunar-archive-plugin gvfs-backends ttf-mscorefonts-installer vlc x11-xserver-utils xdg-desktop-portal xserver-xorg-core nitrogen xclip playerctl xdotool pulseaudio-utils network-manager-gnome ibus lightdm tasksel curl firmware-misc-nonfree wget systemsettings systemd-zram-generator lxappearance initramfs-tools sox libsox-fmt-all lshw lxinput maim nodejs default-jdk python3 gdb bc fail2ban krb5-locales -yy
 
 if lshw -class network | grep -q "wireless"; then
     apt install firmware-iwlwifi -yy
@@ -392,6 +392,7 @@ chown "$USER_NAME":"$USER_NAME" /home/"$USER_NAME" -R
 
 # setup grub
 
+grub-install --target=x86_64-efi
 grub-install --target=x86_64-efi --removable
 update-grub2
 update-initramfs -u -k all
