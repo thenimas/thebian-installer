9ed7ce2362351308a2d73220af776a9f29ea7d39
thenimas <thenimas@hotmail.com>
2026-08-07 00:30:01 -0400

    removed zram in favor of zswap

--- setup.sh
+++ setup.sh
@@ -368,7 +368,7 @@ echo "$HOST_NAME" > /etc/hostname
 hwclock --systohc
 
 # installing packages
-apt install --no-install-suggests --no-install-recommends ark bluez btrfs-progs gh git fonts-recommended fonts-inconsolata fonts-cantarell flatpak gamemode gnome-software ufw i3 kate fastfetch cryptsetup pavucontrol pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse plymouth plymouth-themes qdirstat virt-manager redshift-gtk rxvt-unicode timeshift thunar thunar-archive-plugin gvfs-backends ttf-mscorefonts-installer vlc x11-xserver-utils xdg-desktop-portal xserver-xorg-core xclip playerctl xdotool pulseaudio-utils network-manager-gnome ibus lightdm tasksel curl firmware-misc-nonfree wget systemsettings systemd-zram-generator lxappearance accountsservice sox libsox-fmt-all lshw lxinput maim nodejs default-jdk python3 gdb bc fail2ban krb5-locales firmware-linux linux-headers-amd64 grub-efi-amd64 breeze-cursor-theme xwallpaper geeqie libpam-winbind- apt-listbugs rkhunter lynis apt-listchanges systemd-timesyncd lxqt-policykit ffmpegthumbnailer avahi-utils gvfs-fuse xsettingsd system-config-printer linux-headers-amd64 -yy
+apt install --no-install-suggests --no-install-recommends ark bluez btrfs-progs gh git fonts-recommended fonts-inconsolata fonts-cantarell flatpak gamemode gnome-software ufw i3 kate fastfetch cryptsetup pavucontrol pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse plymouth plymouth-themes qdirstat virt-manager redshift-gtk rxvt-unicode timeshift thunar thunar-archive-plugin gvfs-backends ttf-mscorefonts-installer vlc x11-xserver-utils xdg-desktop-portal xserver-xorg-core xclip playerctl xdotool pulseaudio-utils network-manager-gnome ibus lightdm tasksel curl firmware-misc-nonfree wget systemsettings lxappearance accountsservice sox libsox-fmt-all lshw lxinput maim nodejs default-jdk python3 gdb bc fail2ban krb5-locales firmware-linux linux-headers-amd64 grub-efi-amd64 breeze-cursor-theme xwallpaper geeqie libpam-winbind- apt-listbugs rkhunter lynis apt-listchanges systemd-timesyncd lxqt-policykit ffmpegthumbnailer avahi-utils gvfs-fuse xsettingsd system-config-printer linux-headers-amd64 -yy
 
 wget https://github.com/thenimas/thebian-installer/raw/main/configs/timeshift.json -O /etc/timeshift/timeshift.json
 wget https://github.com/thenimas/thebian-installer/raw/main/configs/jail.local -O /etc/fail2ban/jail.local
@@ -399,13 +399,11 @@ if [ "$INSTALL_TYPE" != 2 ]; then
 fi
 
 wget https://github.com/thenimas/thebian-installer/raw/main/configs/grub -O /etc/default/grub
-wget https://github.com/thenimas/thebian-installer/raw/main/configs/zram-generator.conf -O /etc/systemd/zram-generator.conf
 
 wget https://raw.githubusercontent.com/thenimas/thebian-installer/main/assets/grub-full.png -O /boot/grub/grub-full.png
 wget https://raw.githubusercontent.com/thenimas/thebian-installer/main/assets/grub-wide.png -O /boot/grub/grub-wide.png
 
 systemctl daemon-reload
-systemctl start /dev/zram0
 
 wget https://github.com/thenimas/thebian-installer/raw/main/user.tar -O user.tar
 tar -xf user.tar
